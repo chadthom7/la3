89f9f001d9ec2b23a90a386ec188a17ba2804e1d
chadthom7 <chadthom7@gmail.com>
2019-10-09 15:37:50 -0400

    Thread for send/rec, but w/ IOException: Stream closed

--- chat_client.java
+++ chat_client.java
@@ -1,64 +1,107 @@
-
 /* A Java program for a Client */
+/* Client Must:
+
+
+*/
 import java.net.*; 
 import java.io.*; 
   
 public class chat_client 
 { 
-/* initialize socket and input output streams */
+/* Declare socket and input output streams */
 private Socket socket = null; 
 private BufferedReader input = null; 
 private DataOutputStream out = null;
 private DataInputStream in = null;
+private String o_line = "";
+private String i_line = "";
+// Recieving Thread
+public class ReadingThread extends Thread {
+		// Data from Server
+	ReadingThread() {
+		try {		
+			in = new DataInputStream(
+					new BufferedInputStream(socket.getInputStream()));
+			//	String i_line = "";	
+		} catch(IOException i) {
+			System.out.println(i);
+			System.exit(0); 
+		}
+	}	
+	public void run() {
+		while (!o_line.equals("Over") || !i_line.equals("Over")) { 
+			// Data from server
+			
+			try {
+				Thread.sleep(4000);
+				i_line = in.readUTF();
+				System.out.println("Got input from Server ...");
+				System.out.println("Printing input: "+ i_line);
+			} catch(Exception i) {
+				System.out.println(i);  
+			}// catch (IOException i) {
+			//	System.out.println(i);  
+			//}
+		}
+	}
+}
 
+// Sending Thread
+public class SendingThread extends Thread {
+		// Data from Server
+	SendingThread() {
+		// Initialize buffer readers and input and output streams
+		try { 
+			
+			/* takes user input from terminal */
+			input = new BufferedReader(new InputStreamReader(System.in)); 
+			/* sends output to the socket */
+			out = new DataOutputStream(socket.getOutputStream()); 
+		} catch(IOException i) { 
+			System.out.println(i);
+ 			System.exit(0); 
+		} 
+	}
+	public void run() {
+		/* keep reading until "Over" is input */
+		while (!o_line.equals("Over") || !i_line.equals("Over")) { 
+			try { 
+				Thread.sleep(4000);
+				// Data to send to server
+				o_line = input.readLine(); 
+				out.writeUTF(o_line);
+			} catch(Exception i) { 
+				System.out.println(i); 
+			} 
+		} 
+	}
+}
 /* constructor to put ip address and port */
 public chat_client(String address, int port) 
 { 
-	/* establish a connection */
+	/* Establish a connection */
 	try {
 		socket = new Socket(address, port); 
 	} catch(Exception i) {
 		System.out.println("Error in IP or port");
 		System.exit(0);
-    	}
+  }
 	System.out.println("Connected"); // Connected
-	try { 
-		/* takes input from terminal */
-		input = new BufferedReader(new InputStreamReader(System.in)); 
-
-		/* sends output to the socket */
-		out = new DataOutputStream(socket.getOutputStream()); 
-		
-		// Data from Server
-		in = new DataInputStream(
-			new BufferedInputStream(socket.getInputStream()));
-		
-	
-	} catch(IOException i) { 
-		System.out.println(i); 
-	} 
-	
 	/* string to read message from input */
-	String o_line = ""; 
-	String i_line = "";
+	//String o_line = ""; 
 
-	/* keep reading until "Over" is input */
-	while (!o_line.equals("Over")) { 
-		try { 
-			// Data to send to server
-			o_line = input.readLine(); 
-			out.writeUTF(o_line);
-			//Data from server
-			i_line = in.readUTF();
-			System.out.println("Got input from Server ...");
-			System.out.println("Printing input: "+ i_line);
 
-		} catch(Exception i) { 
-			System.out.println(i); 
-		} 
-	} 
-	
-	/* close the connection */
+
+
+// ------------------- Receive Data ----------------------
+// ------------------- Send Data -------------------------
+
+	ReadingThread r = new ReadingThread();
+	SendingThread s = new SendingThread();
+	new Thread(r).start();
+	new Thread(s).start();
+
+/* close the connection */
 	try { 
 		input.close(); 
 		out.close(); 
@@ -78,5 +121,4 @@ public static void main(String args[])
 		chat_client client = new chat_client(args[0], Integer.parseInt(args[1])); 
 	}
 } 
-
-}
+} // End class chat_client
