7325df34d584eade8be4e87588eac82d1e706738
Carter Venable <cartervenable@Carters-MacBook-Pro-503.local>
2019-10-09 19:55:49 -0400

    reformatted Client.java

--- Client.java
+++ Client.java
@@ -3,11 +3,22 @@
 public class Client {
 	private String name = "";
 	private boolean isBusy;
+	private int portNum = null;
+	private String ip = "";
 
 	//Constructor with name
-	public Client(String aName) {
-		this.name = aName;
+	public Client(int portNum, String ip) {
+		this.name = "";
 		this.isBusy = false;
+		this.portNum = portNum;
+		this.ip = ip;
+	}
+
+	public Client(String name, int portNum, String ip) {
+		this.name = name;
+		this.portNum = portNum;
+		this.ip = ip;
+		this.isBusy = true;
 	}
 
 	public String getClientName() {
@@ -16,6 +27,14 @@ public class Client {
 
 	public boolean getClientState() {
 		return this.isBusy;
+	}
+
+	public int getClientPortNum() {
+		return this.portNum;
+	}
+
+	public String getClientIP() {
+		return this.ip;
 	}
 
 	public void setClientState(boolean isBusy) {
