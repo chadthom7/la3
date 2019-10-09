6aef1bd322f70c90f9d7f26b099a7d3414f34ba7
Carter Venable <cartervenable@Carters-MacBook-Pro-503.local>
2019-10-09 19:47:54 -0400

    reformatted chat_server.java

--- chat_server.java
+++ chat_server.java
@@ -5,15 +5,38 @@ import java.io.*;
 import java.util.*;
 //import client.java;
   
-public class chat_server extends Client
+public class chat_server
 { 
 /* initialize socket and input stream */
 private Socket socket = null; 
 private ServerSocket server = null;
 private BufferedReader input = null;
-private DataInputStream in = null; 
+private DataInputStream in = null;
 private DataOutputStream out = null; 
 private ArrayList<Client> clientList = new ArrayList<Client>();
+private String o_line = "";
+private String i_line = "";
+
+public void newConnection() {
+	
+}
+
+public void runningConnection() {
+	
+}
+
+public class WaitingForConnectionThread extends Thread {
+	WaitingForConnectionThread() {
+		ReadingThread r = new ReadingThread();
+		ForwardingThread f = new ForwardingThread();
+		new Thread(r).start();
+		new Thread(f).start();
+	}
+
+	public void run() {
+		
+	}
+}
 
 public class ReadingThread extends Thread {
 	ReadingThread() {
@@ -24,10 +47,59 @@ public class ReadingThread extends Thread {
 			System.exit(0);
 		}
 	}
+
+	public void run() {
+		while (!socket.isClosed()) {
+			try{
+				i_line = in.readUTF();
+				System.out.println(i_line);
+			} catch(Exception i) {
+				System.out.println(i +"in Reading Thread (Just means" + 
+							" connection Closed)");
+				System.exit(0);
+			}
+		}
+		try {  
+			if(!socket.isClosed())
+				socket.close();
+	    in.close();	
+		} catch(Exception i) {
+			System.out.println(i);  
+		} 
+	}
 }
 
 public class ForwardingThread extends Thread {
+	ForwardingThread() {
+		try {
+			input = new BufferedReader(new InputStreamReader(System.in));
+			out = new DataOutputStream(socket.getOutputStream());
+		} catch(IOException i) {
+			System.out.println(i + " in Sending Thread");
+			System.exit(0);
+		}
+	}
 
+	public void run() {
+		while (!socket.isClosed()) {
+			try {
+				o_line = input.readLine();
+				out.writeUTF(o_line);
+			} catch(Exception i) {
+				System.out.println(i);
+				System.exit(0);
+			}
+		}
+
+		try {
+			if(!socket.isClosed())
+				socket.close();
+			input.close();
+			out.close();
+		} catch(Exception i) {
+			System.out.println(i);
+		}
+	}
 }
 
 
@@ -97,6 +169,8 @@ public static void main(String args[])
 		} catch(Exception i) {
 			System.out.println("Error in port");	
 		}
+		WaitingForConnectionThread c = new WaitingForConnectionThread();
+		new Thread(c).start();
 	}
 } 
 
